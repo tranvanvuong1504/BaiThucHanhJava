38f99fed80b09f5230cd0c5aa4a10ab7f07ede6c
tranvanvuong1504 <2021050747@student.humg.edu.vn>
2022-04-27 14:56:24 +0700

    BaiThucHanh2604

---
+++ BaiThucHanh2604/src/App2604.java
@@ -0,0 +1,15 @@
+import Shape.Triangle;
+import Shape.Point;
+import Shape.Quadrilateral;
+public class App2604 {
+    public static void main(String[] args){
+        Point a = new Point(2, 3);
+        Point b = new Point(0, 3);
+        Point c = new Point(2, 1);
+        Point d = new Point(3, 3);
+        Triangle T = new Triangle(a, b, c);
+        Quadrilateral Q = new Quadrilateral(a, b, c, d);
+        T.checkTriangle();
+        Q.checkQuadrilateral();
+    }
+}
